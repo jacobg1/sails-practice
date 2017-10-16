7775bd932ef2c1a590133c64e0580fcfa453149d
Jacob Greenwald <jeg4t@virginia.edu>
2017-10-16 13:02:13 -0400

    add email validation

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,9 +8,31 @@
 var EmailAddresses = require('machinepack-emailaddresses')
 
 module.exports = {
+
 	// POST action
   create: function createFn (req, res) {
+    var email = req.param('email')
 
+    // validate email
+    EmailAddresses.validate({
+      string: email
+    }).exec({
+
+      // for gen errors
+      error: function (err) {
+        return res.serverError(err)
+      },
+
+      // if email is not validate
+      invalid: function () {
+        return res.badRequest('not a valid email address!')
+      },
+
+      // if email validation is successful, check passwords
+      success: function () {
+
+      }
+    })
   }
 
 }
