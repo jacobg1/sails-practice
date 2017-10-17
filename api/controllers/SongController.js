d6b0f7e5866dde9391726e7bf7047ae92b292d1d
Jacob Greenwald <jeg4t@virginia.edu>
2017-10-16 22:06:12 -0400

    change to song

---
+++ api/controllers/SongController.js
@@ -0,0 +1,10 @@
+/**
+ * SongController
+ *
+ * @description :: Server-side logic for managing Songs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
